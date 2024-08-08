3a57ce56916dcf46a7b9994b65252b772ec328ef
FLAMIN DAVID <146414267+Flamin189@users.noreply.github.com>
2024-08-08 19:20:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './Calculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Basic Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+
+export default App;
